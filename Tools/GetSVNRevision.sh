ce54cb78c4f08a9a3bcf284950f57aaa8a26c3af
mike <mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-01-31 21:58:02 +0000

    Added all the native generation tools (i.e. mine), and a shell script for compiling them.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@13 67207eb2-9daa-044d-9a34-b8567af74898

---
+++ Tools/GetSVNRevision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# GetSVNRevision.sh - Returns the SVN Version of a file, otherwise 0
+
+if [ ! -n "$1" ]
+then
+  echo "Usage: `basename $0` argument1 argument2 etc."
+  exit 1
+fi  
+
+
+for arg in "$@"
+do
+	VAR=$(svn info $arg 2>/dev/null | grep "Last Changed Rev:" | sed s/"Last Changed Rev: "//)
+
+	if [ -n "$VAR" ]
+	then
+		echo "$VAR"
+	else
+		echo "0"
+	fi
+done
+
+exit 0
