7e55c42b4991c5fddb9d6a68e3c9a69e297f94d0
mike <mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-02-02 22:00:49 +0000

    makefile updated, and setup now takes user input for project name.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@28 67207eb2-9daa-044d-9a34-b8567af74898

--- setup.sh
+++ setup.sh
@@ -3,21 +3,15 @@
 
 DEFAULTDIR=Tools/default
 
-if [ ! -n "$1" ]; then
-	echo "USAGE: `basename $0` [GameProjectName] [target_makefile]"
-	echo ""
-	echo "* Project not specified, using defaults..."
-	echo ""
-	
-	PROJECT=`cat $DEFAULTDIR/.project`
-else
-	PROJECT=$1
-fi  
+echo "usage: `basename $0` [GameProjectName] [target_makefile]"
+echo ""
 
 if [ -e ".project" ]; then
-	echo "Warning! File \".project\" exists! Overwrite? y/n [n]"
+	echo "Warning! File \".project\" exists!"
+	echo -n "Overwrite [y/N]? "
 	read answer
-	if [ "$answer" != "y" ]; then
+	if [ "$answer" != "y" ] && [ "$answer" != "Y" ] && [ "$answer" != "yes" ]; then
+		echo "Exiting..."
 		exit 1
 	fi
 fi
@@ -25,22 +19,26 @@ fi
 echo "Generating project settings..."
 echo ""
 
+if [ ! -n "$1" ]; then
+	if [ -e ".project" ]; then
+		PROJECT=`cat .project`
+	else
+		PROJECT=`cat $DEFAULTDIR/.project`
+	fi
+	echo -n "Project Name [$PROJECT]: "
+	read answer
+	if [ "$answer" != "" ]; then
+		PROJECT="$answer"
+	fi
+else
+	PROJECT=$1
+fi  
+
 echo "Project Name:    $PROJECT"
 rm -f .project
 echo "$PROJECT" >.project
 if [ "$WINDIR" != "" ]; then
 	attrib +h .project
-fi
-
-
-# SVN Root Directory #
-REPOS=`Tools/SVNRoot.sh`
-
-echo "Repository Root: $REPOS"
-rm -f .repos
-echo "$REPOS" >.repos
-if [ "$WINDIR" != "" ]; then
-	attrib +h .repos
 fi
 
 
@@ -60,6 +58,17 @@ rm -f .target
 echo "-include $TARGET">.target
 if [ "$WINDIR" != "" ]; then
 	attrib +h .target
+fi
+
+
+# SVN Root Directory #
+REPOS=`Tools/SVNRoot.sh`
+
+echo "Repository Root: $REPOS"
+rm -f .repos
+echo "$REPOS" >.repos
+if [ "$WINDIR" != "" ]; then
+	attrib +h .repos
 fi
 
 # End #
