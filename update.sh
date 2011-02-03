0f9748a0d3ca9a67ac4976db378cdc2bc0d43105
mike <mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-02-03 01:27:03 +0000

    Added two new scripts, commit and update, for performing those actions across all methods.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@44 67207eb2-9daa-044d-9a34-b8567af74898

---
+++ update.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# update.sh - Frontend script for updating all repositories
+
+DEFAULTDIR=Tools/default
+CONFIGDIR=Config
+
+if [ ! -e "$CONFIGDIR/.project" ]; then
+	echo "ERROR: File \".project\" does not exist!  Please run \"setup.sh\" to generate."
+	exit 1
+fi
+
+if [ ! -e "$CONFIGDIR/.repos" ]; then
+	echo "ERROR: File \".repos\" does not exist!  Please run \"setup.sh\" to generate."
+	exit 1
+fi
+
+if [ ! -e "$CONFIGDIR/.content" ]; then
+	echo "ERROR: File \".content\" does not exist!  Please run \"content.sh co\" to generate."
+	exit 1
+fi
+
+if [ ! -e "$CONFIGDIR/.sku" ]; then
+	echo "ERROR: File \".sku\" does not exist!  Please run \"sku.sh co\" to generate."
+	exit 1
+fi
+
+./content.sh update
+
+./sku.sh update
+
+svn update
