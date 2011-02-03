0877e091b7ecb3fd20266e5f3afbb786360f9eea
mike <mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-02-03 00:17:47 +0000

    Added some ignores, and checked-out my "custom" folder to Custom.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@36 67207eb2-9daa-044d-9a34-b8567af74898

--- sku.sh
+++ sku.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 # sku.sh - Script for changing the active SKU (sub-project of an active .target)
 
+DEFAULTDIR=Tools/default
+CONFIGDIR=Config
+
 echo "TODO: This tool"
 
 # checkout (co)
 # update (up)
 # checkin (ci)
 # set - the active sku
+
+#svn co `cat Config/.repos`/custom/`cat Config/.project` Custom
 
 
 exit 0
