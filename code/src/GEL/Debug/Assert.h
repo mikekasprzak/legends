91350eacf9839d3b73b082abe90d4437ef1fc999
mike <mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-08-15 03:02:35 +0000

    Added/updated the log calls.
    git-svn-id: svn://10.0.50.55/legends/trunk@206 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GEL/Debug/Assert.h
+++ code/src/GEL/Debug/Assert.h
@@ -15,37 +15,29 @@
 // Assertion //
 #define Assert( ___TEST, ___STR, ... ) \
 	if ( ___TEST ) { \
-		Log( "- ======================================================== - \n" ); \
-		Log( "Assert Error: " ___STR "\n", ## __VA_ARGS__ ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n", __func__ ); \
-		Log( "- ======================================================== - \n" ); \
-		exit(1); \
-	}
-#define wAssert( ___TEST, ___STR, ... ) \
-	if ( ___TEST ) { \
-		Log( "- ======================================================== - \n" ); \
-		wLog( "Assert Error: " ___STR "\n", ## __VA_ARGS__ ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n", __func__ ); \
-		Log( "- ======================================================== - \n" ); \
+		Log( "- ======================================================== -" ); \
+		Log( "Assert Error: " ___STR, ## __VA_ARGS__ ); \
+		Log( "   Line: %s  File: %s", __LINE__, __FILE__ ); \
+		Log( "   Func: $s", __func__ ); \
+		Log( "- ======================================================== -" ); \
 		exit(1); \
 	}
 // - ------------------------------------------------------------------------------------------ - //
 // Warnings //
 #define Warning( ___TEST, ___STR, ... ) \
 	if ( ___TEST ) { \
-		Log( "- -------------------------------------------------------- - \n" ); \
-		Log( "Warning: " ___STR "\n", ## __VA_ARGS__ ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n\n", __func__ ); \
+		Log( "- -------------------------------------------------------- -" ); \
+		Log( "Warning: " ___STR, ## __VA_ARGS__ ); \
+		Log( "   Line: %s  File: %s", __LINE__, __FILE__ ); \
+		Log( "   Func: $s\n", __func__ ); \
 	}
-#define wWarning( ___TEST, ___STR, ... ) \
-	if ( ___TEST ) { \
-		Log( "- -------------------------------------------------------- - \n" ); \
-		wLog( "Warning: " ___STR "\n", ## __VA_ARGS__ ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n\n", __func__ ); \
+// - ------------------------------------------------------------------------------------------ - //
+// Error Logging //
+#define ELog( ___STR, ... ) \
+	{ \
+		Log( "Error: " ___STR, ## __VA_ARGS__ ); \
+		Log( " Line: %s  File: %s", __LINE__, __FILE__ ); \
+		Log( " Func: $s\n", __func__ ); \
 	}
 // - ------------------------------------------------------------------------------------------ - //
 #else // _MSC_VER //
@@ -53,35 +45,28 @@
 // Assertion //
 #define Assert( ___TEST, ___STR, ___ARGS... ) \
 	if ( ___TEST ) { \
-		Log( "- ======================================================== - \n" ); \
-		Log( "Assert Error: " ___STR "\n", ## __ARGS ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n", __PRETTY_FUNCTION__ ); \
-		Log( "- ======================================================== - \n" ); \
-		exit(1); \
-	}
-#define wAssert( ___TEST, ___STR, ___ARGS... ) \
-	if ( ___TEST ) { \
-		Log( "- ======================================================== - \n" ); \
-		wLog( "Assert Error: " ___STR "\n", ## __ARGS ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n", __PRETTY_FUNCTION__ ); \
-		Log( "- ======================================================== - \n" ); \
+		Log( "- ======================================================== -" ); \
+		Log( "Assert Error: " ___STR, ## __ARGS ); \
+		Log( "   Line: %s  File: %s", __LINE__, __FILE__ ); \
+		Log( "   Func: $s", __PRETTY_FUNCTION__ ); \
+		Log( "- ======================================================== -" ); \
 		exit(1); \
 	}
 // - ------------------------------------------------------------------------------------------ - //
 // Warnings //
 #define Warning( ___TEST, ___STR, ___ARGS... ) \
 	if ( ___TEST ) { \
-		Log( "Warning: " ___STR "\n", ## ___ARGS ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n\n", __PRETTY_FUNCTION__ ); \
+		Log( "Warning: " ___STR, ## ___ARGS ); \
+		Log( "   Line: %s  File: %s", __LINE__, __FILE__ ); \
+		Log( "   Func: $s\n", __PRETTY_FUNCTION__ ); \
 	}
-#define wWarning( ___TEST, ___STR, ___ARGS... ) \
-	if ( ___TEST ) { \
-		wLog( "Warning: " ___STR "\n", ## ___ARGS ); \
-		Log( "   Line: %s  File: %s\n", __LINE__, __FILE__ ); \
-		Log( "   Func: $s\n\n", __PRETTY_FUNCTION__ ); \
+// - ------------------------------------------------------------------------------------------ - //
+// Error Logging //
+#define ELog( ___STR, ___ARGS... ) \
+	{ \
+		Log( "Error: " ___STR, ## ___ARGS ); \
+		Log( " Line: %s  File: %s", __LINE__, __FILE__ ); \
+		Log( " Func: $s\n", __PRETTY_FUNCTION__ ); \
 	}
 // - ------------------------------------------------------------------------------------------ - //
 #endif // _MSC_VER //
@@ -90,8 +75,6 @@
 // - ------------------------------------------------------------------------------------------ - //
 #define Assert( ... ) ;
 #define Warning( ... ) ;
-#define wAssert( ... ) ;
-#define wWarning( ... ) ;
 // - ------------------------------------------------------------------------------------------ - //
 #endif // NDEBUG NOLOGGING //
 // - ------------------------------------------------------------------------------------------ - //
