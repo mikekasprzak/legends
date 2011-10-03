9b61afc18acb7abab662f1041d2c64a7d35a1fd9
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-10-03 19:55:01 +0000

    Added a physics simulation... that does nothing.
    git-svn-id: svn://10.0.50.55/legends/trunk@212 67207eb2-9daa-044d-9a34-b8567af74898

---
+++ code/src/GameLegends/Physics.cpp
@@ -0,0 +1,37 @@
+// - ------------------------------------------------------------------------------------------ - //
+#include "Physics.h"
+#include <Debug/GelDebug.h>
+// - ------------------------------------------------------------------------------------------ - //
+void cPhysics::Init() {
+	Log("+ Creating Bullet Simulation\n");
+	// First we need to pick the broadphase algorithm. I.e. the "are we (maybe) close" check. //
+	// Generally there's 2 options. Dbvt works with any size world, SAP has fixed size world //
+	broadphase = new btDbvtBroadphase();
+	
+	// Configure the main collision detection system (non-broadphase) //
+	collisionConfiguration = new btDefaultCollisionConfiguration();
+	dispatcher = new btCollisionDispatcher( collisionConfiguration );
+	
+	// The above detects, while this the solver solves and resolves collision constraints //
+	solver = new btSequentialImpulseConstraintSolver();
+	
+	
+	// Create our simulation based on the configurations above //
+	dynamicsWorld = new btDiscreteDynamicsWorld( dispatcher, broadphase, solver, collisionConfiguration );
+
+	// Set Gravity //
+	dynamicsWorld->setGravity( btVector3(0,-10,0) );
+}
+// - ------------------------------------------------------------------------------------------ - //
+void cPhysics::Exit() {
+	// Clean up //
+	delete dynamicsWorld;
+	delete solver;
+	delete dispatcher;
+	delete collisionConfiguration;
+	delete broadphase;
+
+	Log("- Done Bullet Simulation\n");
+	Log("\n");
+}
+// - ------------------------------------------------------------------------------------------ - //
