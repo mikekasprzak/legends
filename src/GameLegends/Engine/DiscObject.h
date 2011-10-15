485e159e355ede5ce382f122c7cf1f102a443d8c
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-10-15 21:43:06 +0000

    Now defining DISC objects in a JSON file.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@278 67207eb2-9daa-044d-9a34-b8567af74898

---
+++ src/GameLegends/Engine/DiscObject.h
@@ -0,0 +1,40 @@
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __DiscObject_H__
+#define __DiscObject_H__
+// - ------------------------------------------------------------------------------------------ - //
+#include <Math/Real.h>
+#include <AssetPool/AssetPool.h>
+#include <Graphics/GelColor.h>
+// - ------------------------------------------------------------------------------------------ - //
+class cDiscObject {
+public:
+	GelAssetHandle	Texture;
+	GelColor		Color;
+	
+	GelAssetHandle	GlowTexture;
+	GelAssetHandle	GlowColor;
+
+	
+public:
+	cDiscObject( GelAssetHandle _Texture, GelColor _Color = GEL_RGB_WHITE ) :
+		Texture( _Texture ),
+		Color( _Color ),
+		GlowTexture( _Texture ),
+		GlowColor( _Color )
+	{	
+	}
+	
+	cDiscObject( const char* InFile ) :
+		Texture( 0 ),
+		Color( GEL_RGB_WHITE ),
+		GlowTexture( 0 ),
+		GlowColor( GEL_RGB_WHITE )
+	{
+		LoadFile( InFile );
+	}
+	
+	void LoadFile( const char* InFile );
+};
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __DiscObject_H__ //
+// - ------------------------------------------------------------------------------------------ - //
