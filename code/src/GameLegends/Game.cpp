91f251fe8c8004703e064f6863ccb2f789e1c5c1
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-10-26 19:40:02 +0000

    Refactored the SpaceNavigator stuff out of game.
    git-svn-id: svn://10.0.50.55/legends/trunk@347 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameLegends/Game.cpp
+++ code/src/GameLegends/Game.cpp
@@ -22,7 +22,7 @@
 #include <cJSON.h>
 // - ------------------------------------------------------------------------------------------ - //
 #ifdef USES_HIDAPI
-#include <hidapi.h>
+#include "3DMouse/3DMouse_SpaceNavigator.h"
 #endif // USES_HIDAPI //
 // - ------------------------------------------------------------------------------------------ - //
 #ifdef USES_WINDOWS
@@ -42,126 +42,6 @@ GLuint FBOId;
 int FBOSize = 1024;
 #endif // USES_FBO //
 
-// - ------------------------------------------------------------------------------------------ - //
-#ifdef USES_HIDAPI
-// - ------------------------------------------------------------------------------------------ - //
-hid_device* SpaceNavigator_HID_Handle = 0;
-Real SpaceNavigator[6];
-int SN_DOF[6];
-int SpaceNavigator_Button;
-// - ------------------------------------------------------------------------------------------ - //
-struct SpaceNavigator_Packet {
-	char Type;
-//	short a;
-//	short b;
-//	short c;
-	char a,b,c,d,e,f;
-
-	char PAD;
-	int MORE_PAD[12];
-};// __attribute__((packed));
-// - ------------------------------------------------------------------------------------------ - //
-void HIDAPI_Check() {
-	hid_device_info* Devices;
-	hid_device_info* CurrentDevice;
-
-	Devices = hid_enumerate(0x0, 0x0);
-	CurrentDevice = Devices;	
-	while ( CurrentDevice ) {
-		VLog("");
-		VLog("Device Found!");
-		VLog("  VID PID: %04hx %04hx",	CurrentDevice->vendor_id, CurrentDevice->product_id );
-		VVLog("  path: %s", 				CurrentDevice->path );
-		VLog("  serial_number: %ls",		CurrentDevice->serial_number);
-		VLog("");
-		VLog("  Manufacturer: %ls",		CurrentDevice->manufacturer_string);
-		VLog("  Product:      %ls",		CurrentDevice->product_string);
-		VLog("  Release:      %hx",		CurrentDevice->release_number);
-		VLog("  Interface:    %d",		CurrentDevice->interface_number);
-		VLog("");
-		CurrentDevice = CurrentDevice->next;
-	}
-	hid_free_enumeration( Devices );	
-}
-// - ------------------------------------------------------------------------------------------ - //
-void SpaceNavigator_Init() {
-	SpaceNavigator_HID_Handle = 0;
-	
-	// Check for Space Navigator //
-	hid_device* HIDHandle = hid_open( 0x046d, 0xc626, NULL );
-	if ( HIDHandle ) {
-		int MAX_STR = 256;
-		wchar_t Manufacturer[MAX_STR];
-		wchar_t Product[MAX_STR];
-		//wchar_t SerialNumber[MAX_STR];
-
-		// Just in case, zero //			
-		Manufacturer[0] = 0;
-		Product[0] = 0;
-		//SerialNumber[0] = 0;
-		
-		int Ret;
-		
-		// Get Strings //
-		Ret = hid_get_manufacturer_string(HIDHandle, Manufacturer, MAX_STR);
-		Ret = hid_get_product_string(HIDHandle, Product, MAX_STR);
-		//Ret = hid_get_serial_number_string(HIDHandle, SerialNumber, MAX_STR);
-
-		Log( "* %ls %ls (HID Device) Found!", Manufacturer, Product );
-		
-		hid_set_nonblocking( HIDHandle, 1 );
-
-		SpaceNavigator_HID_Handle = HIDHandle;
-	}
-}
-// - ------------------------------------------------------------------------------------------ - //
-void SpaceNavigator_Update() {
-	if ( SpaceNavigator_HID_Handle ) {
-		SpaceNavigator_Packet Packet;
-		
-		// NOTE: Max seems to be between 340 and 350 per axis limit (+,-) //
-		Real DOF_Scalar(350);
-
-		int Count = 0;
-
-		do {
-			Count = hid_read( SpaceNavigator_HID_Handle, (unsigned char*)&Packet, sizeof(SpaceNavigator_Packet) );
-			if ( Count > 0 ) {
-				if ( Packet.Type == 1 ) {
-					// Translation //
-					SN_DOF[0] = (Packet.a & 0x000000ff) | ((int)Packet.b<<8 & 0xffffff00);
-					SN_DOF[1] = (Packet.c & 0x000000ff) | ((int)Packet.d<<8 & 0xffffff00); 
-					SN_DOF[2] = (Packet.e & 0x000000ff) | ((int)Packet.f<<8 & 0xffffff00);
-
-					SpaceNavigator[0] = Real(SN_DOF[0]) / DOF_Scalar;
-					SpaceNavigator[1] = Real(SN_DOF[1]) / DOF_Scalar;
-					SpaceNavigator[2] = Real(SN_DOF[2]) / DOF_Scalar;
-				}
-				else if ( Packet.Type == 2 ) {
-					// Rotation //
-					SN_DOF[3] = (Packet.a & 0x000000ff) | ((int)Packet.b<<8 & 0xffffff00);
-					SN_DOF[4] = (Packet.c & 0x000000ff) | ((int)Packet.d<<8 & 0xffffff00); 
-					SN_DOF[5] = (Packet.e & 0x000000ff) | ((int)Packet.f<<8 & 0xffffff00);
-
-					SpaceNavigator[3] = Real(SN_DOF[3]) / DOF_Scalar;
-					SpaceNavigator[4] = Real(SN_DOF[4]) / DOF_Scalar;
-					SpaceNavigator[5] = Real(SN_DOF[5]) / DOF_Scalar;
-				}
-				else if ( Packet.Type == 3 ) {
-					// Buttons //
-					SpaceNavigator_Button = Packet.a;
-				}
-				else {
-					Log( "Unknown Packet" );
-				}
-			}
-		}
-		while ( Count > 0 );
-	}
-	//Log( "%i %i %i -- %i %i %i", SN_DOF[0], SN_DOF[1], SN_DOF[2], SN_DOF[3], SN_DOF[4], SN_DOF[5] );
-}
-// - ------------------------------------------------------------------------------------------ - //
-#endif // USES_HIDAPI //
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
@@ -1413,21 +1293,7 @@ void cGame::Draw() {
 #endif // USES_FBO //
 
 #ifdef USES_HIDAPI
-		if ( SpaceNavigator_HID_Handle ) {
-			gelDrawModeFlat();
-			for ( size_t idx = 0; idx < 6; idx++ ) {
-				gelSetColor( GEL_RGB_YELLOW );
-				gelDrawCircle( Vector3D( -150 + (idx * 32), -140, 0), Real(16) );
-	
-				if ( SpaceNavigator[idx] > Real::Zero ) {
-					gelSetColor( GEL_RGB_WHITE );
-				}
-				else {
-					gelSetColor( GEL_RGB_RED );				
-				}
-				gelDrawCircleFill( Vector3D( -150 + (idx * 32), -140, 0), Real(SpaceNavigator[idx] * Real(16)) );
-			}
-		}
+		SpaceNavigator_DrawValues();
 #endif // USES_HIDAPI //
 
 		gelSetColor( GEL_RGB_DEFAULT );
