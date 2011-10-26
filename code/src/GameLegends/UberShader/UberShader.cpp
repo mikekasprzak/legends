f1cce2961ef34ff2b96fe913a3a8c9d73624d471
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-10-26 07:50:29 +0000

    Okay. Searching, Finding, Binding. WE HAVE UBERSHADERS!
    git-svn-id: svn://10.0.50.55/legends/trunk@344 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameLegends/UberShader/UberShader.cpp
+++ code/src/GameLegends/UberShader/UberShader.cpp
@@ -114,8 +114,7 @@ inline void AssignShaderAttributes( const cUberShader_Shader& Program, cJSON* At
 }
 // - ------------------------------------------------------------------------------------------ - //
 cUberShader::cUberShader( const char* InFile ) {
-	// Clear Shader Usage //
-	glUseProgram( 0 );
+	ShaderLookup.clear();
 
 	Log( "+ Loading UberShader Permutations File..." );
 	VLog( "* File: %s", InFile );
@@ -188,7 +187,8 @@ cUberShader::cUberShader( const char* InFile ) {
 				// Link the shader for use //
 				LinkShader( Program );
 				
-				// Add Shader to the UberShader //
+				// Add Program to the UberShader //
+				ShaderLookup[ ShaderObj->string ] = Shader.size();
 				Shader.push_back( Program );
 				
 				// Next Shader //
@@ -212,5 +212,45 @@ cUberShader::cUberShader( const char* InFile ) {
 
 	delete_DataBlock( File );
 	Log( "- Done with UberShader Permutations File." );
+}
+// - ------------------------------------------------------------------------------------------ - //
+cUberShader::~cUberShader() {
+	for ( size_t idx = 0; idx < Shader.size(); idx++ ) {
+		if ( Shader[idx].Program )
+			glDeleteProgram( Shader[idx].Program );
+
+		if ( Shader[idx].Vertex )
+			glDeleteShader( Shader[idx].Vertex );
+		if ( Shader[idx].Fragment )
+			glDeleteShader( Shader[idx].Fragment );
+			
+#ifdef USES_GEOMETRY_SHADER		
+		if ( Shader[idx].Geometry )
+			glDeleteShader( Shader[idx].Geometry );
+#endif // USES_GEOMETRY_SHADER //		
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
+void cUberShader::Bind( const GelShaderHandle Index ) const {
+	glUseProgram( Shader[Index].Program );
+}
+// - ------------------------------------------------------------------------------------------ - //
+GelShaderHandle cUberShader::Find( const char* ShaderName ) {
+	// Search the map for the specific pattern //
+	std::map<std::string, GelShaderHandle>::iterator SearchIterator = ShaderLookup.find( FileName );
+	Log( "+ Searching for Shader \"%s\"", ShaderName );
+	
+	// If it was found, return the Id //
+	if ( SearchIterator != ShaderLookup.end() ) {
+		Log( "- %s found!", ShaderName );
+		return SearchIterator->second;
+	}
+	else {
+		Log( "- ERROR! No Shader named \"%s\"", ShaderName );
+	}
+	
+	return 0;
+
+//	return ShaderLookup[ ShaderName ];
 }
 // - ------------------------------------------------------------------------------------------ - //
