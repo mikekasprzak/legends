81825eabd094c2ffb89d472576d83e82a6207323
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-10-29 17:52:42 +0000

    Committing an inconsequential change to the repos, to test backups. However it's actually a good thing (slight reactor).
    git-svn-id: svn://10.0.50.55/legends/trunk@354 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameLegends/Game.cpp
+++ code/src/GameLegends/Game.cpp
@@ -33,48 +33,6 @@ GelAssetHandle txCursorAttack;
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
-void cGame::InitScripts() {
-	vm_ScriptsLoaded = false;
-
-	// TODO: Compiled Version is ".nut.cnut", or ".nut.cnut.lzma"
-	// Can probably make a call to the GelDirectory and get all the .nut files //
-	
-	Script.push_back( vmScript( "/Main.nut" ) );
-	
-}
-// - ------------------------------------------------------------------------------------------ - //
-void cGame::LoadScripts() {
-	Log( "+ Loading Scripts..." );
-
-	for ( size_t idx = 0; idx < Script.size(); idx++ ) {
-		vm_CompileAndRun( AssetPool::Get( Script[idx].Handle ), Script[idx].FileName );	
-	}
-	vm_ScriptsLoaded = true;
-
-	Log( "- Done Loading Scripts." );
-}
-// - ------------------------------------------------------------------------------------------ - //
-void cGame::ReloadScripts() {
-	if ( vm_ScriptsLoaded ) {				
-		Log( "+ Scanning %i Scripts for changes...", Script.size() );
-
-		for ( size_t idx = 0; idx < Script.size(); idx++ ) {
-			if ( AssetPool::HasChanged( Script[idx].Handle ) ) {
-				Log( "* Change detected in \"%s\". Reloading...", Script[idx].FileName );
-				AssetPool::Reload( Script[idx].Handle );
-				vm_CompileAndRun( AssetPool::Get( Script[idx].Handle ), Script[idx].FileName );
-			}
-		}
-
-		Log( "- Done Scanning Scripts." );
-	}
-	else {
-		ELog( "Scipts haven't been loaded yet!" );
-	}
-}
-// - ------------------------------------------------------------------------------------------ - //
-
-// - ------------------------------------------------------------------------------------------ - //
 void cGame::ContentScan() {
 	TIMEVALUE CurrentScan = GetTimeNow();
 	TIMEVALUE Diff = SubtractTime(CurrentScan, LastContentScan);
