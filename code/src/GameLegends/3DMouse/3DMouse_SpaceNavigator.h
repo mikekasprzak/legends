cb037294d7be2407c47db5e27f0ea3c737196cc0
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-10-31 16:47:33 +0000

    Made the space navigator allow angle tweaking.
    git-svn-id: svn://10.0.50.55/legends/trunk@392 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameLegends/3DMouse/3DMouse_SpaceNavigator.h
+++ code/src/GameLegends/3DMouse/3DMouse_SpaceNavigator.h
@@ -7,6 +7,9 @@ void SpaceNavigator_Init();
 void SpaceNavigator_Update();
 
 void SpaceNavigator_DrawValues();
+
+extern float SpaceNavigator[6];
+extern int SpaceNavigator_Button;
 // - ------------------------------------------------------------------------------------------ - //
 #endif // __3DMOUSE_SPACENAVIGATOR_H__ //
 // - ------------------------------------------------------------------------------------------ - //
