6f417ed97845b06eaf256017c8d367054bcf4eb6
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-11-13 01:21:00 +0000

    camera is fixed for now, investingating segfault.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@424 67207eb2-9daa-044d-9a34-b8567af74898

--- src/GameLegends/Game.cpp
+++ src/GameLegends/Game.cpp
@@ -413,19 +413,19 @@ void cGame::Init() {
 	Obj3.back()->GlowColor = GEL_RGBA(64,64,0,64);
 	Obj3.back()->IsGlowing = true;
 
-	AddObject3D( Vector3D( 4+4, 4, 4+2 ), "/Skull.pme", Real(0.8) );
+//	AddObject3D( Vector3D( 4+4, 4, 4+2 ), "/Skull.pme", Real(0.8) );
 	AddObject3D( Vector3D( 4+8, 4, 4+2 ), "/Bottle.pme", Real(0.8) );
 
 	AddObject3D( Vector3D( 4, 4+4, 4+2 ), "/Book_Open.pme", Real(0.8) );
 	AddObject3D( Vector3D( 4+4, 4+4, 4+2 ), "/Shield.pme", Real(0.6) );
-	AddObject3D( Vector3D( 4+8, 4+4, 4+2 ), "/Shield.pme", Real(0.8) );
+//	AddObject3D( Vector3D( 4+8, 4+4, 4+2 ), "/Shield.pme", Real(0.8) );
 	AddObject3D( Vector3D( 4, 4+8, 4+2 ), "/CandleStick.pme", Real(0.8) );
 	AddObject3D( Vector3D( 4+4, 8+4, 4+2 ), "/Key_Boxy.pme", Real(0.4) );
 	
-	AddObject3D( Vector3D( 4+4, 8+4, 4+2 ), "/Key_Boxy.pme", Real(0.8) );
+//	AddObject3D( Vector3D( 4+4, 8+4, 4+2 ), "/Key_Boxy.pme", Real(0.8) );
 //	AddObject3D( Vector3D( 4, 4+8, 4 ), "/Table_SquareCenter.pme", Real(0.8) );
 	AddObject3D( Vector3D( -14, 4, 8+4 ), "/Sword.pme", Real(2) );
-	AddObject3D( Vector3D( 0, -6, 8+2 ), "/Sword.pme", Real(1) );
+//	AddObject3D( Vector3D( 0, -6, 8+2 ), "/Sword.pme", Real(1) );
 //	Obj3.back()->IsGlowing = true;
 
 	AddOldRoomMesh( Vector3D( 0, 0, 0 ), "/RockTest.pme" );
