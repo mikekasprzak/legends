b82212b614e284db46b440b3fa5b12a247a1f84b
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2011-11-27 01:57:51 +0000

    Added some utility shell scripts.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@459 67207eb2-9daa-044d-9a34-b8567af74898

---
+++ Target/BlackBerry/connect-ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ssh devuser@192.168.1.223 -i MySSHKey
+
+exit 0
--- Target/BlackBerry/connect.sh
+++ Target/BlackBerry/connect.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+
 blackberry-connect -targetHost 192.168.1.223 -devicePassword thunder -sshPublicKey MySSHKey.pub
+
+exit 0
