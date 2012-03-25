99d2ebc8cd22ebd72cac9b2adc83f5ae7bb3d983
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-03-25 05:01:02 +0000

    Amazing! I have my C++ tree code running in javascript!
    git-svn-id: svn://10.0.50.55/legends/trunk@491 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/Emscripten/Main.cpp
+++ code/src/Emscripten/Main.cpp
@@ -1,16 +1,32 @@
 // - ------------------------------------------------------------------------------------------ - //
 #include <stdio.h>
+#include <time.h>
 #include "Gel2D/GelGraphics2D.h"
 // - ------------------------------------------------------------------------------------------ - //
+#include "../GameLegends/TreeForge/TFTree.h"
+// - ------------------------------------------------------------------------------------------ - //
+
+cTFTree* MyTree;
 
 // - ------------------------------------------------------------------------------------------ - //
 int main( int argc, char* argv[] ) {
 	gelGraphicsInit( 640, 480 );
 	
-	gelSetColor( 255, 0, 0, 255 );
-	gelDrawCircleFill( 240, 240, 32 );
+	//gelSetColor( 255, 0, 0, 255 );
+	//gelDrawCircleFill( 240, 240, 32 );
 	
 	//gelDrawFillRect( 64, 64, 320-128, 128 );
+
+
+	{
+		srand ( time(NULL) );
+		int Blah = rand(); // Burn a random number //
+	}
+
+	cTFGenerator TreeGen;
+	MyTree = TreeGen.Generate();
+
+	MyTree->Draw();
 
 	gelGraphicsExit();
 	
--- code/src/Emscripten/make.sh
+++ code/src/Emscripten/make.sh
@@ -8,5 +8,5 @@ echo "// Begin PreJS.txt //">obj/PreJS.txt
 cat GelHTML/GelUtil.js>>obj/PreJS.txt
 cat GelHTML/GelGraphics2D.js>>obj/PreJS.txt
 
-~/Code/emscripten/emcc -O2 Main.cpp --pre-js obj/PreJS.txt -o output/Sugar.js 
+~/Code/emscripten/emcc -D NOT_GCC -I ../GEL -O2 Main.cpp ../GEL/Math/Real.cpp ../GEL/Math/Vector/Vector3D.cpp --pre-js obj/PreJS.txt -o output/Sugar.js 
 
