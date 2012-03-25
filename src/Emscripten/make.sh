95808bdc9cf5ba098adc2f67c4cb9efe4d3ae12a
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-03-25 05:33:57 +0000

    Tree Gen in Javascript working nicely.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@493 67207eb2-9daa-044d-9a34-b8567af74898

--- src/Emscripten/make.sh
+++ src/Emscripten/make.sh
@@ -8,5 +8,6 @@ echo "// Begin PreJS.txt //">obj/PreJS.txt
 cat GelHTML/GelUtil.js>>obj/PreJS.txt
 cat GelHTML/GelGraphics2D.js>>obj/PreJS.txt
 
-~/Code/emscripten/emcc -D NOT_GCC -I ../GEL -O2 Main.cpp ../GEL/Math/Real.cpp ../GEL/Math/Vector/Vector3D.cpp --pre-js obj/PreJS.txt -o output/Sugar.js 
+~/Code/emscripten/emcc -D NOT_GCC -I ../GEL -O2 -s EXPORTED_FUNCTIONS='["__Z6NewGenv", "_main"]' Main.cpp ../GEL/Math/Real.cpp ../GEL/Math/Vector/Vector3D.cpp --pre-js obj/PreJS.txt -o output/Sugar.js 
 
+rm a.out
