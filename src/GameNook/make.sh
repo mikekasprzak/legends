f9923d49aeb3d989f681dcebb758d17d21a04a87
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-04-21 23:51:19 +0000

    Fixed scrolling at Richards. Added Nook Animations.
    git-svn-id: svn://10.0.50.55/legends/trunk/code@507 67207eb2-9daa-044d-9a34-b8567af74898

--- src/GameNook/make.sh
+++ src/GameNook/make.sh
@@ -18,6 +18,6 @@ cat Load.js>>obj/PreJS.txt
 
 #$CC -D NOT_GCC -I ../GEL -O2 -s EXPORTED_FUNCTIONS='["__Z6NewGenv", "_main"]' Main.cpp ../GEL/Math/Real.cpp ../GEL/Math/Vector/Vector3D.cpp --pre-js obj/PreJS.txt -o output/Sugar.js 
 
-$CC -D NOT_GCC -D EMSCRIPTEN -D USES_UNIX_DIR -I ../GEL -I ../External/cJSON -O2 src/Main.cpp ../GEL/Math/Real.cpp ../GEL/Debug/LogEmscripten.cpp ../GEL/Math/Vector/Vector3D.cpp ../External/cJSON/cJSON.c --pre-js obj/PreJS.txt --embed-file Content/MapData.json -o output/nook.js 
+$CC -D NOT_GCC -D EMSCRIPTEN -D USES_UNIX_DIR -I ../GEL -I ../External/cJSON -O0 src/Main.cpp ../GEL/Math/Real.cpp ../GEL/Debug/LogEmscripten.cpp ../GEL/Math/Vector/Vector3D.cpp ../External/cJSON/cJSON.c --pre-js obj/PreJS.txt --embed-file Content/MapData.json -o output/nook.js 
 
 #rm a.out
