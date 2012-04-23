db4b956075a6851bc26d85a7768988e5fa42a1fd
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-04-23 00:52:20 +0000

    I dunno anymore.
    git-svn-id: svn://10.0.50.55/legends/trunk@529 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameNook/make.sh
+++ code/src/GameNook/make.sh
@@ -3,6 +3,11 @@
 #export CC='~/Code/emscripten/emcc'
 export CC='/d/Build/em2/emcc'
 
+export CFLAGS='-O0 --closure 0'
+export DEFINES='-D NOT_GCC -D EMSCRIPTEN -D USES_UNIX_DIR'
+export INCLUDES='-I ../GEL -I ../External/cJSON'
+export FILES='src/Main.cpp ../GEL/Math/Real.cpp ../GEL/Debug/LogEmscripten.cpp ../GEL/Math/Vector/Vector3D.cpp ../External/cJSON/cJSON.c'
+
 mkdir -p obj output
 
 echo "// Begin PreJS.txt //">obj/PreJS.txt
@@ -21,6 +26,6 @@ cat Load.js>>obj/PreJS.txt
 
 #$CC -D NOT_GCC -I ../GEL -O2 -s EXPORTED_FUNCTIONS='["__Z6NewGenv", "_main"]' Main.cpp ../GEL/Math/Real.cpp ../GEL/Math/Vector/Vector3D.cpp --pre-js obj/PreJS.txt -o output/Sugar.js 
 
-$CC -D NOT_GCC -D EMSCRIPTEN -D USES_UNIX_DIR -I ../GEL -I ../External/cJSON -O0 src/Main.cpp ../GEL/Math/Real.cpp ../GEL/Debug/LogEmscripten.cpp ../GEL/Math/Vector/Vector3D.cpp ../External/cJSON/cJSON.c --pre-js obj/PreJS.txt --embed-file Content/MapData.json -o output/nook.js 
+$CC $DEFINES $INCLUDES $CFLAGS $FILES --pre-js obj/PreJS.txt --embed-file Content/MapData.json -o output/nook.js 
 
 #rm a.out
