59f4f78dbac43a42c661d718db0fccb3049cad08
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-07-18 22:36:07 +0000

    Some minor tweaks to the animal. Adding an alternative way of representing awake hours (as a bitmask).
    git-svn-id: svn://10.0.50.55/legends/trunk@637 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameSurvive/Engine/WorldAnimal.h
+++ code/src/GameSurvive/Engine/WorldAnimal.h
@@ -2,6 +2,7 @@
 #ifndef __WORLDANIMAL_H__
 #define __WORLDANIMAL_H__
 // - ------------------------------------------------------------------------------------------ - //
+#include <Util/Binary.h>
 #include "WorldObject.h"
 // - ------------------------------------------------------------------------------------------ - //
 class cWorldAnimal: public cWorldObject {
@@ -28,6 +29,29 @@ public:
 	static int CtMatutinal[];//	= { 1,1,0,0,0,0,1,1 };
 	static int CtVespertine[];//= { 0,0,1,1,1,1,0,0 };
 	static int CtBimodal[];//	= { 1,0,0,1,1,0,0,1 };
+	
+	//int ChronoMask;	// Hours of the day I am awake -- 24 hour //
+	enum {
+		CM_DAY			= (b11111111 << 16) | (b11110000 << 8) | b00000000,
+		CM_NIGHT		= (b00000000 << 16) | (b00001111 << 8) | b11111111,
+		
+		CM_DAWN			= (b00000000 << 16) | (b00000000 << 8) | b00000001,
+		CM_SUNRISE		= (b10000000 << 16) | (b00000000 << 8) | b00000000,
+		CM_SUNSET		= (b00000000 << 16) | (b00010000 << 8) | b00000000,
+		CM_DUSK			= (b00000000 << 16) | (b00001000 << 8) | b00000000,		
+		
+		CM_DEAD			= 0,
+		CM_DIURNAL		= CM_DAY,
+		CM_NOCTURNAL	= CM_NIGHT,
+		CM_INSOMNIAC	= CM_DIURNAL | CM_NOCTURNAL,
+		
+		// Not entirely accurate (twilights only), but I prefer it to be a 2 hour period //
+		CM_MATUTINAL	= CM_DAWN | CM_SUNRISE,
+		CM_VESPERTINE	= CM_DUSK | CM_SUNSET,
+		CM_BIMODAL		= CM_MATUTINAL | CM_VESPERTINE,
+		
+		CM_MASK			= (b11111111 << 16) | (b11111111 << 8) | b11111111
+	};
 public:
 	cWorldAnimal() 
 	{
