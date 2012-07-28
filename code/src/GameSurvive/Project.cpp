0962b4f924dcf397effb8457573463545bf4b6a5
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-07-28 02:14:40 +0000

    New project file settings. Removed Atom Store. Goodbye.
    git-svn-id: svn://10.0.50.55/legends/trunk@671 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameSurvive/Project.cpp
+++ code/src/GameSurvive/Project.cpp
@@ -4,7 +4,7 @@
 
 // - ------------------------------------------------------------------------------------------ - //
 #define PRODUCT_NAME	"Life Formula"
-#define PRODUCT_VERSION	"Release 1"
+#define PRODUCT_VERSION	"WIP 1"
 //#define PRODUCT_VERSION	"0.1"
 // - ------------------------------------------------------------------------------------------ - //
 
@@ -12,46 +12,30 @@
 // OS SUFFIX //
 #if defined(USES_WINDOWS)
 #ifdef _MSC_VER
-#define OS_SUFFIX		"(Windows MSVC Build)"
+#ifdef USES_DIRECT3D
+#ifdef USES_METRO
+#define OS_SUFFIX		"(Metro Direct3D)"
+#else // USES_METRO //
+#define OS_SUFFIX		"(Windows Direct3D MSVC)"
+#endif // USES_METRO //
+#else // USES_DIRECT3D //
+#define OS_SUFFIX		"(Windows OpenGL MSVC)"
+#endif // USES_DIRECT3D //
 #else // _MSC_VER //
-#define OS_SUFFIX		"(Windows MinGW Build)"
+#define OS_SUFFIX		"(Windows OpenGL MinGW)"
 #endif // _MSC_VER //
 #elif defined(USES_LINUX)
-
-#ifdef USES_ATOMSTORE
-#define OS_SUFFIX		"(Moblin Build)"
-#else // USES_ATOMSTORE //
-#define OS_SUFFIX		"(Linux Build)"
-#endif // USES_ATOMSTORE //
-
+#define OS_SUFFIX		"(Linux OpenGL)"
 #else // OS SUFFIX //
 #define OS_SUFFIX	""
 #endif // OS SUFFIX//
 // - ------------------------------------------------------------------------------------------ - //
 // STORE SUFFIX //
-#if defined(USES_ATOMSTORE)
-#define STORE_SUFFIX	" -- Intel Atom SE"
+#if defined(USES_STEAMWORKS)
+#define STORE_SUFFIX	" -- Steam"
 #else // STORE SUFFIX //
 #define STORE_SUFFIX	""
 #endif // STORE SUFFIX //
-// - ------------------------------------------------------------------------------------------ - //
-
-// - ------------------------------------------------------------------------------------------ - //
-#ifdef USES_ATOMSTORE
-// - ------------------------------------------------------------------------------------------ - //
-#include <AtomStore/adpcore.h>
-// - ------------------------------------------------------------------------------------------ - //
-extern const ADP_APPLICATIONID myApplicationID;
-
-#if defined(USES_WINDOWS)
-const ADP_APPLICATIONID myApplicationID = {{
-	0xAC201A91,0xFE4F4AFC,0x99BE4683,0x1A554C63 }};
-#else defined(USES_LINUX)
-const ADP_APPLICATIONID myApplicationID = {{
-	0x45BDFB64,0x892240C7,0xB6D2DCDF,0xE2C3361C }};
-#endif // USES_WINDOWS/LINUX //
-// - ------------------------------------------------------------------------------------------ - //
-#endif // USES_ATOMSTORE //
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
