52a6dd86408b56cf6f8247bef72e4f7c82d3cea7
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-08-14 16:20:19 +0000

    Shader update... ha, all I had to do was comment out a line. Tee hee!
    git-svn-id: svn://10.0.50.55/legends/trunk@744 67207eb2-9daa-044d-9a34-b8567af74898

--- content/GameSurvive/Scripts/glsl/TileMesh.glsl
+++ content/GameSurvive/Scripts/glsl/TileMesh.glsl
@@ -13,7 +13,6 @@ precision highp float;
 uniform mat4 ViewMatrix;
 uniform vec3 FaceCenter;
 uniform vec2 UVPos;
-//uniform float Scalar;
 
 //uniform vec3 FaceNormal;
 //const vec3 FaceCenter = vec3(0,0,0);
@@ -28,18 +27,18 @@ varying vec4 var_Color1;
 varying vec4 var_Color2;
 
 void main() {
-	vec3 Pos = VertexPos;// * Scalar;
+	vec3 Pos = VertexPos;
 	gl_Position = ViewMatrix * vec4( Pos, 1 );	// Manually making it a homogeneous vector //
 
 	var_TexCoord = (VertexPos.xy + UVPos + VertexPos.zz) * (1.0 / 16.0);
 	var_Color1 = Color1 * (1.0/255.0);
-//	var_Color2 = Color2 * (1.0/255.0);
+	var_Color2 = Color2 * (1.0/255.0);
 //	var_Color2 = vec4(0.75);
 
 //	var_Color2 = vec4( dot( Normal, FaceNormal ) ); // Darker as angle changes, no proximity //
 	
-	vec3 ToCenter = FaceCenter - VertexPos;
-	var_Color2 = vec4( min(1.0 + dot( Normal, normalize(ToCenter) ), 1.0) );
+//	vec3 ToCenter = FaceCenter - VertexPos;
+//	var_Color2 = vec4( min(1.0 + dot( Normal, normalize(ToCenter) ), 1.0) );
 }
 // - ------------------------------------------------------------------------------------------ - //
 #endif // VERTEX_SHADER //
