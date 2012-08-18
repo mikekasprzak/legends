d56409d0c08b789667c43118438d5281568674a8
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-08-18 06:32:02 +0000

    Now enabling anisotropic filtering. Makes the mipmaps tolerable.
    git-svn-id: svn://10.0.50.55/legends/trunk@756 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GEL/Graphics/Texture/PVR3Texture_Load.cpp
+++ code/src/GEL/Graphics/Texture/PVR3Texture_Load.cpp
@@ -227,6 +227,12 @@ GelTexture_NativeHandle load_PVR3Texture( PVR3Texture* Texture, GelTexture::GelT
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+			float MaxAnisotropy;
+			glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &MaxAnisotropy);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, MaxAnisotropy );
+			
+//			Log( "> MaxAnisotropy: %f", MaxAnisotropy );
+			
 			// Bilinear //
 //			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 //			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
