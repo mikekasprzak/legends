de4557101a9e6dbc29be1dabde1570deb96d873a
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-10-19 07:52:00 +0000

    Static libraries should now work.
    git-svn-id: svn://10.0.50.55/legends/trunk@776 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/ServerSatellite/make.sh
+++ code/src/ServerSatellite/make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -I /usr/local/include -I ../GEL/ -I ../External/ main.cpp ../External/cJSON/cJSON.c -o SatServ.exe `curl-config --libs`
+g++ -DCURL_STATICLIB -I /usr/local/include -I ../GEL/ -I ../External/ main.cpp ../External/cJSON/cJSON.c -o SatServ.exe `curl-config --static-libs`
