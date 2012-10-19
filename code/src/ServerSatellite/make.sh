78ba8de31620885955602b7269c7c4efe0d5b5ef
mike <mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-10-19 19:51:06 +0000

    werd up yo
    git-svn-id: svn://10.0.50.55/legends/trunk@782 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/ServerSatellite/make.sh
+++ code/src/ServerSatellite/make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -DCURL_STATICLIB -I /usr/local/include -I ../GEL/ -I ../External/ main.cpp NetGet.cpp GelGeoData.cpp ../External/cJSON/cJSON.c -o SatServ.exe `curl-config --static-libs`
+g++ -DCURL_STATICLIB -I /usr/local/include -I ../GEL/ -I ../External/ main.cpp NetGet.cpp GELGeoData.cpp ../External/cJSON/cJSON.c -o SatServ.exe `curl-config --static-libs`
