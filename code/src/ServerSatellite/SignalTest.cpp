66dcc8242675e440f38b272e46fbe35783b1c024
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-10-23 00:38:36 +0000

    teh
    git-svn-id: svn://10.0.50.55/legends/trunk@801 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/ServerSatellite/SignalTest.cpp
+++ code/src/ServerSatellite/SignalTest.cpp
@@ -4,14 +4,19 @@
 #include <signal.h>
 
 void term_func( int Signal ) {
-	printf( "SIGTERM recieved -- %i\n", Signal );
-	fflush(0);
+	printf( "SIGTERM (Terminate) recieved -- %i\n", Signal );
+	exit(1);
+}
+
+void tstop_func( int Signal ) {
+	printf( "SIGTSTP (Terminal Stop) recieved (CTRL+Z) -- %i\n", Signal );
 	exit(1);
 }
 
 int main( int argc, char* argv[] ) {
 	
 	signal( SIGTERM, term_func );
+	signal( SIGTSTP, tstop_func );
 
 	printf( "Waiting for a kill signal...\n" );
 	fflush(0);
