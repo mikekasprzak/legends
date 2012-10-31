cc7bf292a7aa4bdc5ab13d2ffb4eb90c1a215b84
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-10-31 22:05:22 +0000

    update make.sh, so Redhat/Centos can build.
    git-svn-id: svn://10.0.50.55/legends/trunk@882 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/ServerSatellite/make.sh
+++ code/src/ServerSatellite/make.sh
@@ -3,17 +3,17 @@
 DEFINES="-DMONGOOSE_NO_WIN32_INIT -DCURL_STATICLIB"
 INCLUDES="-I /usr/local/include -I /usr/local/ssl/include -I ../GEL/ -I ../External/"
 FILES="main.cpp NetGet.cpp GELGeoData.cpp ../GEL/Debug/Log.cpp ../External/cJSON/cJSON.c ../External/Mongoose/mongoose.c NetAdapter/NetAdapter_*.cpp"
-LIBS="`curl-config --static-libs` -L /usr/local/ssl/lib -lcrypto"
+LIBS="-L /usr/local/ssl/lib -lcrypto"
 
 SYSTEM=`uname`
 
 # If $WINDIR is NOT set #
 if [ -n "$WINDIR" ]; then
-	LIBS="$LIBS -lIphlpapi -static"
+	LIBS="$LIBS `curl-config --static-libs` -lIphlpapi -static"
 elif [ "$SYSTEM" == "FreeBSD" ]; then
-	LIBS="$LIBS -lpthread"
+	LIBS="$LIBS `curl-config --libs` -lpthread"
 else
-	LIBS="$LIBS -ldl"
+	LIBS="$LIBS `curl-config --libs` -ldl"
 fi  
 
 g++ $DEFINES $INCLUDES $FILES -o SatServ.exe $LIBS
