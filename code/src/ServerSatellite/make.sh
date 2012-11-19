ca4c2691f8cc997c72da37a8d7b9ee5bbff176b6
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-11-19 04:13:52 +0000

    hope
    git-svn-id: svn://10.0.50.55/legends/trunk@931 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/ServerSatellite/make.sh
+++ code/src/ServerSatellite/make.sh
@@ -8,7 +8,7 @@ DEFINES="-DMONGOOSE_NO_WIN32_INIT -DCURL_STATICLIB"
 INCLUDES="-I /usr/local/include -I /usr/local/ssl/include -I ../GEL/ -I ../External/ -I ../External/ENet/include -I ."
 CFILES="Util/*.c ../External/cJSON/*.c ../External/Mongoose/*.c"
 CPPFILES="*.cpp Net/*.cpp Sat/*.cpp Text/*.cpp ../External/TinyThread/*.cpp ../GEL/Debug/Log.cpp ../GEL/Timer/*.cpp ../GEL/Math/*.cpp ../GEL/Math/Vector/*.cpp ../GEL/Math/Matrix/*.cpp NetAdapter/*.cpp"
-LIBS="-L /usr/local/ssl/lib -lcrypto -L /usr/local/lib -lenet"
+LIBS="-L /usr/local/ssl/lib -lcrypto -L /usr/local/lib"
 
 # apt-get install ncurses-dev
 
@@ -19,16 +19,16 @@ SYSTEM=`uname`
 # If $WINDIR is NOT set #
 if [ -n "$WINDIR" ]; then
 	INCLUDES="$INCLUDES -I ../External/PDCurses/include"
-	LIBS="$LIBS ../External/PDCurses/pdcurses.lib `curl-config --static-libs` -lIphlpapi -lwinmm -static"
+	LIBS="$LIBS ../External/PDCurses/pdcurses.lib `curl-config --static-libs` -lIphlpapi -lwinmm -lenet -static"
 elif [ "$SYSTEM" = "FreeBSD" ]; then
 	INCLUDES="$INCLUDES"
 	DEFINES="$DEFINES -DUSES_UNIX_TIMER"
-	LIBS="$LIBS `curl-config --libs` -lpthread -lncurses"
+	LIBS="$LIBS `curl-config --libs` -lpthread -lncurses libenet.a"
 	FLAGS="$FLAGS -Wl,-rpath,$ORIGIN"
 else
 	INCLUDES="$INCLUDES"
 	DEFINES="$DEFINES -DUSES_UNIX_TIMER"
-	LIBS="$LIBS `curl-config --libs` -ldl -lncurses"
+	LIBS="$LIBS `curl-config --libs` -ldl -lncurses libenet.a"
 	FLAGS="$FLAGS -Wl,-rpath,$ORIGIN"
 fi  
 
