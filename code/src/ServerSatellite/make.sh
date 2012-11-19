ae74d9bbd1b938bac8ce255c86e63c213d765c46
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-11-19 03:51:00 +0000

    Wrong curse.
    git-svn-id: svn://10.0.50.55/legends/trunk@925 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/ServerSatellite/make.sh
+++ code/src/ServerSatellite/make.sh
@@ -4,11 +4,13 @@
 # "--std=c++0x" causes failures with _fileno and other legacy macros. Solution: Use gnu++0x #
 
 FLAGS="--std=gnu++0x"
-DEFINES="-DMONGOOSE_NO_WIN32_INIT -DCURL_STATICLIB -DUSES_UNIX_TIMER"
-INCLUDES="-I /usr/local/include -I /usr/local/ssl/include -I ../GEL/ -I ../External/ -I ../External/PDCurses/include -I ../External/ENet/include -I ."
+DEFINES="-DMONGOOSE_NO_WIN32_INIT -DCURL_STATICLIB"
+INCLUDES="-I /usr/local/include -I /usr/local/ssl/include -I ../GEL/ -I ../External/ -I ../External/ENet/include -I ."
 CFILES="Util/*.c ../External/cJSON/*.c ../External/Mongoose/*.c"
 CPPFILES="*.cpp Net/*.cpp Sat/*.cpp Text/*.cpp ../External/TinyThread/*.cpp ../GEL/Debug/*.cpp ../GEL/Timer/*.cpp ../GEL/Math/*.cpp ../GEL/Math/Vector/*.cpp ../GEL/Math/Matrix/*.cpp NetAdapter/*.cpp"
 LIBS="-L /usr/local/ssl/lib -lcrypto -L /usr/local/lib -lenet -lwinmm"
+
+# apt-get install ncurses-dev
 
 SYSTEM=`uname`
 
@@ -16,10 +18,15 @@ SYSTEM=`uname`
 
 # If $WINDIR is NOT set #
 if [ -n "$WINDIR" ]; then
+	INCLUDES="$INCLUDES -I ../External/PDCurses/include"
 	LIBS="$LIBS ../External/PDCurses/pdcurses.lib `curl-config --static-libs` -lIphlpapi -static"
 elif [ "$SYSTEM" = "FreeBSD" ]; then
+	INCLUDES="$INCLUDES"
+	DEFINES="$DEFINES -DUSES_UNIX_TIMER"
 	LIBS="$LIBS `curl-config --libs` -lpthread"
 else
+	INCLUDES="$INCLUDES"
+	DEFINES="$DEFINES -DUSES_UNIX_TIMER"
 	LIBS="$LIBS `curl-config --libs` -ldl"
 fi  
 
