d013f76f6f6c3fe544ba64e670b77a1a25710795
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-11-22 19:50:20 +0000

    I can ping the serverer now.
    git-svn-id: svn://10.0.50.55/legends/trunk@935 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameSatellite/App_Client.cpp
+++ code/src/GameSatellite/App_Client.cpp
@@ -30,9 +30,9 @@ bool cApp::Client_Connect() {
 	ENetAddress Address;
 	ENetEvent Event;
 
-//	enet_address_set_host( &Address, "foagies.mooo.com" );
-	enet_address_set_host( &Address, "127.0.0.1" );
+//	enet_address_set_host( &Address, "127.0.0.1" );
 //	enet_address_set_host( &Address, "192.168.1.111" );
+	enet_address_set_host( &Address, "foagies.mooo.com" );
 	Address.port = 10240;
 
 	Client_Peer = enet_host_connect(
