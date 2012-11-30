9350edfb0da7ca7b3b9e5ddb7a8636dcc409900a
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-11-30 10:28:02 +0000

    Assert Merged in to Debug Logging.
    git-svn-id: svn://10.0.50.55/legends/trunk@970 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GEL/Debug/Assert.h
+++ code/src/GEL/Debug/Assert.h
@@ -9,56 +9,73 @@
 // - ------------------------------------------------------------------------------------------ - //
 #ifndef NDEBUG
 // - ------------------------------------------------------------------------------------------ - //
-// Assertion //
-#define Assert( ___TEST, ... ) \
-	{if ( ___TEST ) { \
-		LogAlways( "-========================================================-" ); \
-		_LogAlways( "! Assert Error: " ); \
-		LogAlways( __VA_ARGS__ ); \
-		LogAlways( "   Line: %i  File: %s", __LINE__, __FILE__ ); \
-		LogAlways( "   Func: %s", __PRETTY_FUNCTION__ ); \
-		LogAlways( "-========================================================-" ); \
-		::exit(1); \
-	}}
+// AssertFlush -- This is what does the actual exiting //
+#define AssertFlush() \
+	{ \
+		extern bool _AssertFlush; \
+		if ( _AssertFlush ) { \
+			::exit(1); \
+		} \
+	}
 // - ------------------------------------------------------------------------------------------ - //
-// Warnings //
-#define Warning( ___TEST, ... ) \
-	{if ( ___TEST ) { \
+// Assert without Flush -- On success, tell the user the program is in terrible shape //
+#define _Assert( __TEST, ... ) \
+	{ \
+		if ( __TEST ) { \
+			LogAlways( "-========================================================-" ); \
+			_LogAlways( "! Assert Error: " ); \
+			LogAlways( __VA_ARGS__ ); \
+			LogAlways( "   Line: %i  File: %s", __LINE__, __FILE__ ); \
+			LogAlways( "   Func: %s", __PRETTY_FUNCTION__ ); \
+			\
+			extern bool _AssertFlush; \
+			_AssertFlush = true; \
+		} \
+	}
+// - ------------------------------------------------------------------------------------------ - //
+// Assert -- On success, Tell the user the program is in terrible shape, then exit //
+#define Assert( __TEST, ... ) \
+	{ \
+		_Assert( __TEST, __VA_ARGS__ ); \
+		AssertFlush(); \
+	}
+
+// - ------------------------------------------------------------------------------------------ - //
+// Just the Warning text. Do not use! //
+#define __WarningText( ... ) \
+	{ \
 		LogAlways( "-========================================================-" ); \
 		_LogAlways( "! Warning: " ); \
 		LogAlways( __VA_ARGS__ ); \
 		LogAlways( "   Line: %i  File: %s", __LINE__, __FILE__ ); \
 		LogAlways( "   Func: %s\n", __PRETTY_FUNCTION__ ); \
-	}}
+	}
 // - ------------------------------------------------------------------------------------------ - //
-// Warnings (return) //
-#define return_Warning( ___TEST, ... ) \
-	{if ( ___TEST ) { \
-		LogAlways( "-========================================================-" ); \
-		_LogAlways( "! Warning: " ); \
-		LogAlways( __VA_ARGS__ ); \
-		LogAlways( "   Line: %i  File: %s", __LINE__, __FILE__ ); \
-		LogAlways( "   Func: %s\n", __PRETTY_FUNCTION__ ); \
-		return; \
-	}}
-// - ------------------------------------------------------------------------------------------ - //
-// Warnings (return value) //
-#define return_value_Warning( ___VAL, ___TEST, ... ) \
-	{if ( ___TEST ) { \
-		LogAlways( "-========================================================-" ); \
-		_LogAlways( "! Warning: " ); \
-		LogAlways( __VA_ARGS__ ); \
-		LogAlways( "   Line: %i  File: %s", __LINE__, __FILE__ ); \
-		LogAlways( "   Func: %s\n", __PRETTY_FUNCTION__ ); \
-		return ___VAL; \
-	}}
+#define Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); } }
+#define return_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { __WarningText( __VA_ARGS__ ); return __ERROR; } }
+#define return_void_Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return; } }
+#define return_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return __RETCODE; } }
+
+#define if_Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); } }
+#define return_if_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { __WarningText( __VA_ARGS__ ); return __ERROR; } }
+#define return_if_void_Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return; } }
+#define return_if_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return __RETCODE; } }
 // - ------------------------------------------------------------------------------------------ - //
 #else // NDEBUG //
 // - ------------------------------------------------------------------------------------------ - //
 #define Assert( ... ) ;
+#define _Assert( ... ) ;
+#define AssertFlush( ... ) ;
+
 #define Warning( ... ) ;
-#define return_Warning( ... ) ;
-#define return_value_Warning( ... ) ;
+#define return_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { return __ERROR; } }
+#define return_void_Warning( __TEST, ... ) { if ( __TEST ) { return; } }
+#define return_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { return __RETCODE; } }
+
+#define if_Warning( ... ) ;
+#define return_if_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { return __ERROR; } }
+#define return_if_void_Warning( __TEST, ... ) { if ( __TEST ) { return; } }
+#define return_if_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { return __RETCODE; } }
 // - ------------------------------------------------------------------------------------------ - //
 #endif // NDEBUG //
 // - ------------------------------------------------------------------------------------------ - //
--- code/src/GEL/Debug/Log.h
+++ code/src/GEL/Debug/Log.h
@@ -281,7 +281,7 @@ extern const char* GetMLogData();
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
-// Debug Build Only Logging //
+// Debug Build Only //
 // - ------------------------------------------------------------------------------------------ - //
 #if !defined(NDEBUG) && !defined(NO_LOGGING)
 // - ------------------------------------------------------------------------------------------ - //
@@ -290,12 +290,79 @@ extern const char* GetMLogData();
 #define wDLog( ... ) { wLogAlways( __VA_ARGS__ ); }
 #define _wDLog( ... ) { _wLogAlways( __VA_ARGS__ ); }
 // - ------------------------------------------------------------------------------------------ - //
+extern bool _AssertFlush;
+// - ------------------------------------------------------------------------------------------ - //
+// AssertFlush -- This is what does the actual exiting //
+#define AssertFlush() \
+	{ \
+		if ( _AssertFlush ) { \
+			::exit(1); \
+		} \
+	}
+// - ------------------------------------------------------------------------------------------ - //
+// Assert without Flush -- On success, tell the user the program is in terrible shape //
+#define _Assert( __TEST, ... ) \
+	{ \
+		if ( __TEST ) { \
+			LogAlways( "-========================================================-" ); \
+			_LogAlways( "! Assert Error: " ); \
+			LogAlways( __VA_ARGS__ ); \
+			LogAlways( "   Line: %i  File: %s", __LINE__, __FILE__ ); \
+			LogAlways( "   Func: %s", __PRETTY_FUNCTION__ ); \
+			\
+			_AssertFlush = true; \
+		} \
+	}
+// - ------------------------------------------------------------------------------------------ - //
+// Assert -- On success, Tell the user the program is in terrible shape, then exit //
+#define Assert( __TEST, ... ) \
+	{ \
+		_Assert( __TEST, __VA_ARGS__ ); \
+		AssertFlush(); \
+	}
+
+// - ------------------------------------------------------------------------------------------ - //
+// Just the Warning text. Do not use! //
+#define __WarningText( ... ) \
+	{ \
+		LogAlways( "-========================================================-" ); \
+		_LogAlways( "! Warning: " ); \
+		LogAlways( __VA_ARGS__ ); \
+		LogAlways( "   Line: %i  File: %s", __LINE__, __FILE__ ); \
+		LogAlways( "   Func: %s\n", __PRETTY_FUNCTION__ ); \
+	}
+// - ------------------------------------------------------------------------------------------ - //
+#define Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); } }
+#define return_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { __WarningText( __VA_ARGS__ ); return __ERROR; } }
+#define return_void_Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return; } }
+#define return_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return __RETCODE; } }
+
+#define if_Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); } }
+#define return_if_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { __WarningText( __VA_ARGS__ ); return __ERROR; } }
+#define return_if_void_Warning( __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return; } }
+#define return_if_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { __WarningText( __VA_ARGS__ ); return __RETCODE; } }
+// - ------------------------------------------------------------------------------------------ - //
 #else // NDEBUG //
 // - ------------------------------------------------------------------------------------------ - //
 #define DLog( ... ) ;
 #define _DLog( ... ) ;
 #define wDLog( ... ) ;
 #define _wDLog( ... ) ;
+
+#define Assert( ... ) ;
+#define _Assert( ... ) ;
+#define AssertFlush() ;
+
+#define Warning( ... ) ;
+#define return_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { return __ERROR; } }
+#define return_void_Warning( __TEST, ... ) { if ( __TEST ) { return; } }
+#define return_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { return __RETCODE; } }
+
+// For Syntax Compatibility //
+#define if_Warning( ... ) ;
+#define return_if_Warning( __TEST, ... ) { if ( auto __ERROR = (__TEST) ) { return __ERROR; } }
+#define return_if_void_Warning( __TEST, ... ) { if ( __TEST ) { return; } }
+#define return_if_value_Warning( __RETCODE, __TEST, ... ) { if ( __TEST ) { return __RETCODE; } }
 // - ------------------------------------------------------------------------------------------ - //
 #endif // NDEBUG //
 // - ------------------------------------------------------------------------------------------ - //
