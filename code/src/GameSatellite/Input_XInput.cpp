b29df978690853e73870a73105f639b881da8954
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-11-30 19:20:26 +0000

    Heavily refactored GameSatellite main. Now many files.
    git-svn-id: svn://10.0.50.55/legends/trunk@975 67207eb2-9daa-044d-9a34-b8567af74898

---
+++ code/src/GameSatellite/Input_XInput.cpp
@@ -0,0 +1,97 @@
+// - ------------------------------------------------------------------------------------------ - //
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <XInput/XInput.h>
+#include "Input_XInput.h"
+// - ------------------------------------------------------------------------------------------ - //
+#include <Debug/Log.h>
+// - ------------------------------------------------------------------------------------------ - //
+namespace XInput {
+// - ------------------------------------------------------------------------------------------ - //
+// Reference: http://code.google.com/p/xcontroller/
+// MSDN: http://msdn.microsoft.com/en-us/library/windows/desktop/microsoft.directx_sdk.reference.xinputenable%28v=vs.85%29.aspx
+// - ------------------------------------------------------------------------------------------ - //
+bool Connected[XUSER_MAX_COUNT];
+bool OldConnected[XUSER_MAX_COUNT];
+
+XINPUT_STATE State[XUSER_MAX_COUNT];
+XINPUT_CAPABILITIES Caps[XUSER_MAX_COUNT];
+XINPUT_BATTERY_INFORMATION Battery[XUSER_MAX_COUNT];
+XINPUT_BATTERY_INFORMATION HeadsetBattery[XUSER_MAX_COUNT];
+XINPUT_VIBRATION Vibration[XUSER_MAX_COUNT];
+// - ------------------------------------------------------------------------------------------ - //
+void Init() {
+	ZeroMemory( &Connected, sizeof(Connected) );
+	ZeroMemory( &OldConnected, sizeof(OldConnected) );
+	
+	ZeroMemory( &State, sizeof(State) );
+	ZeroMemory( &Vibration, sizeof(Vibration) );
+	
+	// TODO: Add the Focus Lost and Gained events via a signal
+}
+// - ------------------------------------------------------------------------------------------ - //
+void LoseFocus( void* ) {
+	XInputEnable( FALSE );
+}
+// - ------------------------------------------------------------------------------------------ - //
+void GainFocus( void* ) {
+	XInputEnable( TRUE );
+}
+// - ------------------------------------------------------------------------------------------ - //
+void Poll() {
+	for ( int idx = 0; idx < XUSER_MAX_COUNT; idx++ ) {
+		OldConnected[idx] = Connected[idx];
+		
+		Connected[idx] = (XInputGetState(idx, &State[idx]) == ERROR_SUCCESS);
+		// ERROR_SUCCESS, ERROR_DEVICE_NOT_CONNECTED
+
+		if( Connected[idx] ) {
+			XInputGetCapabilities( idx, 0, &Caps[idx] );
+			XInputGetBatteryInformation( idx, BATTERY_DEVTYPE_GAMEPAD, &Battery[idx] );
+			XInputGetBatteryInformation( idx, BATTERY_DEVTYPE_HEADSET, &HeadsetBattery[idx] );
+			XInputSetState( idx, &Vibration[idx] );
+		}
+	}
+	
+	// TODO: KeyStrokes
+}	
+// - ------------------------------------------------------------------------------------------ - //
+size_t Size() {
+	return XUSER_MAX_COUNT;
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+size_t DevicesConnected() {
+	size_t Count = 0;
+	for ( size_t idx = 0; idx < XUSER_MAX_COUNT; idx++ ) {
+		if( IsConnected(idx) ) {
+			Count++;
+		}
+	}
+	return Count;
+}
+// - ------------------------------------------------------------------------------------------ - //
+bool IsConnected( const int Index ) {
+	return Connected[Index];
+}
+bool IsConnected() {
+	for ( int idx = 0; idx < XUSER_MAX_COUNT; idx++ ) {
+		return_if( IsConnected(idx) );
+	}
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+bool HasConnectionChanged( const int Index ) {
+	return OldConnected[Index] != Connected[Index];
+}
+bool HasConnectionChanged() {
+	for ( int idx = 0; idx < XUSER_MAX_COUNT; idx++ ) {
+		return_if( HasConnectionChanged(idx) );
+	}
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+}; // namespace XInput //
+// - ------------------------------------------------------------------------------------------ - //
---
+++ code/src/GameSatellite/main_SDLInit.cpp
@@ -0,0 +1,79 @@
+// - ------------------------------------------------------------------------------------------ - //
+#include <SDL2/SDL.h>
+#include <gl/gl.h>
+// - ------------------------------------------------------------------------------------------ - //
+#include <Debug/Log.h>
+// - ------------------------------------------------------------------------------------------ - //
+#include "main_SDLInit.h"
+// - ------------------------------------------------------------------------------------------ - //
+void ReportSDLVersion() {
+	SDL_version ver;
+	SDL_VERSION(&ver);
+	Log( "SDL (core) - v%u.%u.%u -- Compiled Version", ver.major, ver.minor, ver.patch );
+
+	SDL_version pver;
+	SDL_GetVersion(&pver);
+	Log( "SDL (core) - v%u.%u.%u -- Linked Version (DLL or Shared Library)", pver.major, pver.minor, pver.patch );
+	Log( "" );
+
+	int LinkVer = (ver.major << 16) | (ver.minor << 8) | (ver.patch << 0);
+	int DLLVer = (pver.major << 16) | (pver.minor << 8) | (pver.patch << 0);
+
+	if ( LinkVer > DLLVer ) {
+		Log( "* WARNING: Linked version is older than Compiled version!!" );
+		Log( "  If you have problems starting the game, this may be why." );
+		Log( "  Upgrade to a newer SDL version to resolve this." );
+		Log( "" );
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
+void ReportSDLGraphicsInfo() {
+	{
+		// NOTE: Not very useful. Number of drivers compiled in to SDL. //
+		VLog( "-=- SDL Video Drivers (not very useful) -=-" );
+		for( int idx = 0; idx < SDL_GetNumVideoDrivers(); idx++ ) {			
+			VLog( "%i - %s", idx, SDL_GetVideoDriver( idx ) );
+		}
+		VLog("");
+	}
+	
+	{
+		Log( "-=- Video Displays -- %i Device(s) Connected -=-", SDL_GetNumVideoDisplays() );
+		for( int idx = 0; idx < SDL_GetNumVideoDisplays(); idx++ ) {
+			SDL_DisplayMode Mode;
+			SDL_GetDesktopDisplayMode( idx, &Mode );
+
+			SDL_Rect Rect;
+			SDL_GetDisplayBounds( idx, &Rect );
+			
+			Log( "%i - %i, %i at %i Hz [%x] -- Location: %i, %i (%i,%i)", 
+				idx, 
+				Mode.w, Mode.h, Mode.refresh_rate, Mode.format, 
+				Rect.x, Rect.y, Rect.w, Rect.h 
+				);
+		}
+		Log("");
+	}	
+}
+// - ------------------------------------------------------------------------------------------ - //
+const char* SDL_WindowEventName( Uint8 EventName ) {
+	switch( EventName ) {
+		case SDL_WINDOWEVENT_NONE:			return "SDL_WINDOWEVENT_NONE";
+		case SDL_WINDOWEVENT_SHOWN:			return "SDL_WINDOWEVENT_SHOWN";
+		case SDL_WINDOWEVENT_HIDDEN:		return "SDL_WINDOWEVENT_HIDDEN";
+		case SDL_WINDOWEVENT_EXPOSED:		return "SDL_WINDOWEVENT_EXPOSED";
+		case SDL_WINDOWEVENT_MOVED:			return "SDL_WINDOWEVENT_MOVED";
+		case SDL_WINDOWEVENT_RESIZED:		return "SDL_WINDOWEVENT_RESIZED";
+		case SDL_WINDOWEVENT_SIZE_CHANGED:	return "SDL_WINDOWEVENT_SIZE_CHANGED";
+		case SDL_WINDOWEVENT_MINIMIZED:		return "SDL_WINDOWEVENT_MINIMIZED";
+		case SDL_WINDOWEVENT_MAXIMIZED:		return "SDL_WINDOWEVENT_MAXIMIZED";
+		case SDL_WINDOWEVENT_RESTORED:		return "SDL_WINDOWEVENT_RESTORED";
+		case SDL_WINDOWEVENT_ENTER:			return "SDL_WINDOWEVENT_ENTER";
+		case SDL_WINDOWEVENT_LEAVE:			return "SDL_WINDOWEVENT_LEAVE";
+		case SDL_WINDOWEVENT_FOCUS_GAINED:	return "SDL_WINDOWEVENT_FOCUS_GAINED";
+		case SDL_WINDOWEVENT_FOCUS_LOST:	return "SDL_WINDOWEVENT_FOCUS_LOST";
+		case SDL_WINDOWEVENT_CLOSE:			return "SDL_WINDOWEVENT_CLOSE";
+	};
+	return "SDL_WINDOWEVENT_???";
+}
+// - ------------------------------------------------------------------------------------------ - //
