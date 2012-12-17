7919bee02b03bc6617dc20e40373c2090cb7f25d
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-12-17 09:20:46 +0000

    closer and closer.
    git-svn-id: svn://10.0.50.55/legends/trunk@1046 67207eb2-9daa-044d-9a34-b8567af74898

---
+++ code/src/MainServer/Main/Main_Curses.cpp
@@ -0,0 +1,79 @@
+// - ------------------------------------------------------------------------------------------ - //
+#include <Debug/Log.h>
+#include <Timer/Timer.h>
+
+#include "App.h"
+#include <Util/util_curses.h>
+#include "Text/Out.h"
+// - ------------------------------------------------------------------------------------------ - //
+#include "Main_Product.h"
+#include "Main_SVNVersion.h"
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+#include <signal.h>
+// - ------------------------------------------------------------------------------------------ - //
+void term_func( int Signal ) {
+	ELog( "SIGTERM (Terminate) recieved -- %i", Signal );
+	LogFlush();
+	exit(1);
+}
+// - ------------------------------------------------------------------------------------------ - //
+void int_func( int Signal ) {
+	ELog( "\nSIGINT (Interrupt) recieved (CTRL+C) -- %i", Signal );
+	LogFlush();
+	exit(1);
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+int main( int argc, char* argv[] ) {	
+	if ( !curses_IsSupported() ) {
+		printf( "ERROR: PDCurses is not supported. :(\n\n");
+		printf( "Hey, let information@sykhronics.com know you got this error and you could win a\n" );
+		printf( "prize!! OK not really, but we really want to know if anyone gets this. Thanks!\n\n" );
+		printf( "Solution: Run this from an ordinary CMD shell, or install and use WinPTY.\n" );
+		printf( "https://github.com/rprichard/winpty -- \"console SatServ\"\n" );
+
+		return 1;
+	}
+
+	// **** //
+
+	LogInit();
+	
+	signal( SIGTERM, term_func );
+	signal( SIGINT, int_func );
+
+	// **** //
+
+	Log( "-=======- GEL2 Server Started -- Curses Branch -- SKU: %s -=======-", PRODUCT_SKU );
+	Log( "GEL (C) Copyright 2008-2013 Mike Kasprzak and Sykhronics Entertainment" );
+	Log( "" );
+	Log( "-=- SKU: %s -=- %s -=-", PRODUCT_SKU, FullProductName );
+
+	OutInit();	
+	
+	// **** //
+	
+	gelNetInit();	// Mainly for WinSock //
+	
+	// **** //
+	
+	extern void SatBodyTest();
+	SatBodyTest();
+	
+	// **** //
+	
+	{
+		cApp App;
+		App();
+		
+		//Log( "Uptime: %s", System::GetClockString() );
+	}
+
+	OutExit();
+
+	return 0;
+}
+// - ------------------------------------------------------------------------------------------ - //
