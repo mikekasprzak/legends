f9da15feeb3bf0f7d65abbb8ab5d6d84d1113dc6
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-12-24 16:03:12 +0000

    Thing now has same members an Player. Fixed the missing >> and << operators in Integer and SInteger.
    git-svn-id: svn://10.0.50.55/legends/trunk@1068 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GameSatellite/Game_Thing.h
+++ code/src/GameSatellite/Game_Thing.h
@@ -2,15 +2,40 @@
 #ifndef __BOAT_GAME_THING_H__
 #define __BOAT_GAME_THING_H__
 // - ------------------------------------------------------------------------------------------ - //
+#include <Math/SVector.h>
+// - ------------------------------------------------------------------------------------------ - //
 namespace BOAT {
 // - ------------------------------------------------------------------------------------------ - //
 class cThing {
 public:
+	SVector2D Pos;
+
+public:
 	cThing()
+	{
+	}
+
+	cThing( const int x, const int y ) :
+		Pos( x, y )
 	{
 	}
 	
 public:
+	// Which Region //
+	inline const size_t GetRegionX() const {
+		return Pos.x >> 3; // div 8 //
+	}
+	inline const size_t GetRegionY() const {
+		return Pos.y >> 3; // div 8 //
+	}
+
+	// The Position inside the Region //
+	inline const size_t GetRegionPosX() const {
+		return Pos.x & 7; // mod 8 //
+	}
+	inline const size_t GetRegionPosY() const {
+		return Pos.y & 7; // mod 8 //
+	}
 	
 };
 // - ------------------------------------------------------------------------------------------ - //
