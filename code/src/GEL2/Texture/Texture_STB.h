3ef6318318dd76f44828c80e9630104ab466f58f
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-12-26 21:04:38 +0000

    Added some functions for converting STBTextures in to Grid2D's. Can convert to 32bit, 16bit, and 8bit (RGBA8888, RGBA4444, RGBA2222).
    git-svn-id: svn://10.0.50.55/legends/trunk@1102 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GEL2/Texture/Texture_STB.h
+++ code/src/GEL2/Texture/Texture_STB.h
@@ -4,6 +4,7 @@
 // - ------------------------------------------------------------------------------------------ - //
 #include <string.h>
 #include <Texture/TextureHandle.h>
+#include <Grid/Grid2D_Class.h>
 // - ------------------------------------------------------------------------------------------ - //
 namespace Texture {
 // - ------------------------------------------------------------------------------------------ - //
@@ -24,8 +25,76 @@ TextureHandle upload_STBTexture( STBTexture& Texture );				// Load an STBTexture
 // NOTE: If you want Width, Height, or Info, you should retrieve it directly from //
 //   the STBTexture type, before deleting the STBTexture (i.e. Texture.Width, etc) //
 // - ------------------------------------------------------------------------------------------ - //
-// template<class T=int>
-// toGrid2D_STBTexture
+inline cGrid2D<unsigned> to_32bit_Grid2D_STBTexture( STBTexture& Texture ) {
+	typedef unsigned tType;
+	cGrid2D<tType> Grid;
+	
+	Grid.SetW( Texture.Width );
+	Grid.SetH( Texture.Height );
+	tType* Data = new tType[ Texture.Width * Texture.Height ];
+	Grid.SetData( Data );
+
+	unsigned* SrcData = (unsigned*)Texture.Data;
+		
+	for ( int h = 0; h < Texture.Height; h++ ) {
+		for ( int w = 0; w < Texture.Width; w++ ) {
+			size_t Index = w + (h * Texture.Width);
+			Data[Index] = SrcData[Index];
+		}
+	}
+	
+	return Grid;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline cGrid2D<unsigned short> to_16bit_Grid2D_STBTexture( STBTexture& Texture ) {
+	typedef unsigned short tType;
+	cGrid2D<tType> Grid;
+	
+	Grid.SetW( Texture.Width );
+	Grid.SetH( Texture.Height );
+	tType* Data = new tType[ Texture.Width * Texture.Height ];
+	Grid.SetData( Data );
+
+	unsigned char* SrcData = (unsigned char*)Texture.Data;
+		
+	for ( int h = 0; h < Texture.Height; h++ ) {
+		for ( int w = 0; w < Texture.Width; w++ ) {
+			size_t Index = w + (h * Texture.Width);
+			Data[Index] = 
+				((SrcData[(Index<<2)+0]>>4) << 0) | 
+				((SrcData[(Index<<2)+1]>>4) << 4) | 
+				((SrcData[(Index<<2)+2]>>4) << 8) | 
+				((SrcData[(Index<<2)+3]>>4) << 12);
+		}
+	}
+	
+	return Grid;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline cGrid2D<unsigned char> to_8bit_Grid2D_STBTexture( STBTexture& Texture ) {
+	typedef unsigned char tType;
+	cGrid2D<tType> Grid;
+	
+	Grid.SetW( Texture.Width );
+	Grid.SetH( Texture.Height );
+	tType* Data = new tType[ Texture.Width * Texture.Height ];
+	Grid.SetData( Data );
+
+	unsigned char* SrcData = (unsigned char*)Texture.Data;
+		
+	for ( int h = 0; h < Texture.Height; h++ ) {
+		for ( int w = 0; w < Texture.Width; w++ ) {
+			size_t Index = w + (h * Texture.Width);
+			Data[Index] = 
+				((SrcData[(Index<<2)+0]>>6) << 0) | 
+				((SrcData[(Index<<2)+1]>>6) << 2) | 
+				((SrcData[(Index<<2)+2]>>6) << 4) | 
+				((SrcData[(Index<<2)+3]>>6) << 6);
+		}
+	}
+	
+	return Grid;
+}
 // - ------------------------------------------------------------------------------------------ - //
 }; // namespace Texture //
 // - ------------------------------------------------------------------------------------------ - //
