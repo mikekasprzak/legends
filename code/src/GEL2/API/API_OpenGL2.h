5cc0e511692920b20d430bc3baf6ec6fa20f3751
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-12-26 13:44:11 +0000

    Hide some warnings about GL on Windows.
    git-svn-id: svn://10.0.50.55/legends/trunk@1090 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GEL2/API/API_OpenGL2.h
+++ code/src/GEL2/API/API_OpenGL2.h
@@ -3,6 +3,15 @@
 #define __GEL_API_OPENGL2_H__
 // - ------------------------------------------------------------------------------------------ - //
 #include <External/GLEE/GLee.h>
+
+#ifdef _WIN32
+	// Hide some Warnings on Windows //
+	#undef GL_LINES_ADJACENCY_EXT
+	#undef GL_LINE_STRIP_ADJACENCY_EXT
+	#undef GL_TRIANGLES_ADJACENCY_EXT
+	#undef GL_TRIANGLE_STRIP_ADJACENCY_EXT
+#endif // _WIN32
+
 #include <gl/gl.h>
 // - ------------------------------------------------------------------------------------------ - //
 #endif // __GEL_API_OPENGL2_H__ //
