a8dc990e0f20dd785f1e6e6a408a5c1a1d81f260
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-12-30 15:15:50 +0000

    Lots and lots of input devices.
    git-svn-id: svn://10.0.50.55/legends/trunk@1122 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GEL2/Input/Input_Device_3DMouse.h
+++ code/src/GEL2/Input/Input_Device_3DMouse.h
@@ -1,18 +1,8 @@
 // - ------------------------------------------------------------------------------------------ - //
-#ifndef __GEL_INPUT_H__
-#define __GEL_INPUT_H__
+#ifndef __GEL_INPUT_3DMOUSE_H__
+#define __GEL_INPUT_3DMOUSE_H__
 // - ------------------------------------------------------------------------------------------ - //
-#include "Input_Core.h"
-
-//#include "Input_Device_Keyboard.h"
-//#include "Input_Device_Mouse.h"
-#include "Input_Device_Accelerometer.h"
-#include "Input_Device_Joystick.h"
-#include "Input_Device_Camera.h"
-#include "Input_Device_DepthCamera.h"
-#include "Input_Device_MotionController.h"
-#include "Input_Device_3DMouse.h"
 
 // - ------------------------------------------------------------------------------------------ - //
-#endif // __GEL_INPUT_H__ //
+#endif // __GEL_INPUT_3DMOUSE_H__ //
 // - ------------------------------------------------------------------------------------------ - //
