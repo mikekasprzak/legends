5278549d080671fbc1c9b225f0e65b08333c9829
Mike <Mike@67207eb2-9daa-044d-9a34-b8567af74898>
2012-12-30 15:23:32 +0000

    Renaming Joystick to Controller, since some Joysticks have multiple joysticks (Xbox controller, Steel Battalion).
    git-svn-id: svn://10.0.50.55/legends/trunk@1123 67207eb2-9daa-044d-9a34-b8567af74898

--- code/src/GEL2/Input/Input_Device_Joystick.h
+++ code/src/GEL2/Input/Input_Device_Joystick.h
@@ -1,8 +1,8 @@
 // - ------------------------------------------------------------------------------------------ - //
-#ifndef __GEL_INPUT_DEVICE_JOYSTICK_H__
-#define __GEL_INPUT_DEVICE_JOYSTICK_H__
+#ifndef __GEL_INPUT_DEVICE_CONTROLLER_H__
+#define __GEL_INPUT_DEVICE_CONTROLLER_H__
 // - ------------------------------------------------------------------------------------------ - //
-#include "Input_Device_Joystick_XInput.h"
+#include "Input_Device_Controller_XInput.h"
 // - ------------------------------------------------------------------------------------------ - //
-#endif // __GEL_INPUT_DEVICE_JOYSTICK_H__ //
+#endif // __GEL_INPUT_DEVICE_CONTROLLER_H__ //
 // - ------------------------------------------------------------------------------------------ - //
